7f582d0542cd3eeabb0756d7a73684180f4f04aa
Felipe Vilela <felipe.vilela01@outlook.com>
2024-10-23 21:39:37 -0300

    chore: initial project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import './App.css'
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+
+    return (
+      
+  )
+}
+
+export default App
+
