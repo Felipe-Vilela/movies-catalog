7dd004e2e17443d71377ee7251333d2f9ec14c41
Felipe Vilela <felipe.vilela01@outlook.com>
2024-10-28 15:46:16 -0300

    feat: create and style footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="flex justify-around items-center relative top-0 w-full bg-zinc-900 p-4 z-10">
+      <p className="text-lg text-slate-200">
+        Desenvolvido por <span className="font-bold">Cauã Olivio</span> e{" "}
+        <span className="font-bold">Felipe Vilela</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
